82bc75c10331134b187b06e041d9e26d1f73c737
Reysmer Valle <reysmerwvr@belatrixsf.com>
2019-10-03 17:11:43 -0500

    [In Progress] - Terraform AWS with Elastic Beanstalk

--- demo-aws-autoscaling/vars.tf
+++ demo-aws-autoscaling/vars.tf
@@ -1,7 +1,11 @@
-variable "AWS_ACCESS_KEY" {}
-variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
   default = "us-east-1"
+}
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
 }
 variable "AMIS" {
   type = "map"
